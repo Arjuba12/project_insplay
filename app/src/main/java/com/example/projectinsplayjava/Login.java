fca9df6ea84675d2186309e6c91c28736abba5f0
Arjuna Bimantara <arjunabimantara91@gmail.com>
2024-09-30 19:42:08 +0700

    v0.0.2 -Membuat intro app

--- app/src/main/java/com/example/projectinsplayjava/Login.java
+++ app/src/main/java/com/example/projectinsplayjava/Login.java
@@ -1,6 +1,11 @@
 package com.example.projectinsplayjava;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+    private EditText userEdit, passEdit;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,30 @@ public class Login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        initValue();
+
+    }
+
+    private void initValue() {
+        userEdit = findViewById(R.id.email_form);
+        passEdit = findViewById(R.id.pass_form);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userEdit.getText().toString().isEmpty() || passEdit.getText().toString().isEmpty()){
+                    Toast.makeText(Login.this, "Username dan Password Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+                }else if(userEdit.getText().toString().equals("Test") && passEdit.getText().toString().equals("Test123")){
+                    Intent intent;
+                    intent = new Intent(Login.this, Intro.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Login.this, "Username dan Password Salah", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
